cb668d272324839b2c0865872010a53fe779ba7c
debarghabhattofficial <dbhattacharjee@monarchtractor.com>
2023-10-31 14:13:31 +0530

    Renamed train.sh to test.sh.
    Renamed train.sh to test.sh as it we are not training but FastMOT agent, but testing it on our custom input video.

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Change to ROOT directory.
+cd ..
+
+# Execute the python script.
+python app.py \
+    --input-uri "input/wobble.avi" \
+    --config "cfg/mot-osnet_x0_25_msmt17.json" \
+    --mot
+
+
+
+
+
